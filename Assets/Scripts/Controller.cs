c3b58635af6db3318ed9c94761cdb59ac52c2572
ThyRedRock <kolicannon@gmail.com>
2026-03-10 09:02:45 -0400

    banana breakout game [unfixed]

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,153 @@
+﻿/* Unity Game Programming
+ * Banana Breakout
+ * Author name
+ * Project date
+ */
+
+using UnityEngine;
+using TMPro;
+
+public class Controller : MonoBehaviour 
+{
+	// public variables lets the user easily
+	// control the number of blocks and rows
+	public int blocksPerRow = 8;
+	public int numRows = 3;
+
+	// public variable for text widget
+	public TextMeshProUGUI gameOverText;
+
+	// public array holding references to the prefab blocks
+	public GameObject[] blockPrefabs;
+
+	// index into blockPrefabs array
+	private int currentPrefab = 0;
+
+	// calculated values to display the chosen
+	// number of rows and columns across the screen
+	private float startingX;
+	private float startingY;
+	private float xOffset;
+	private float yOffset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// reset current prefab index and game over text
+		currentPrefab = 0;
+		gameOverText.text = "";
+
+		// create the first set of blocks
+		createBlocks ();
+	}
+
+	void Update()
+	{
+	}
+
+	// This public function can be called by other scripts
+	// (MonkeyScript) to see if all the blocks are gone and,
+	// if so, regenerate the next set of blocks.
+	public bool CheckRestart()
+	{
+        // if there are not any blocks left
+		if (!anyBlocksLeft())
+		{
+			// create new blocks
+			createBlocks ();
+			return true; // signal restart
+		}
+		return false; // signal no restart
+	}
+
+	// This function will create a new set of blocks
+	// using the current prefab index.  It will also
+	// increment the prefab index to the next spot in
+	// the prefab array.
+	void createBlocks()
+	{
+		// safety check in case the prefab array is not initialized
+		if (blockPrefabs.Length == 0) 
+		{
+			return;	// no blocks to create
+		}
+
+		// get the current prefab object
+		GameObject prefab = blockPrefabs [currentPrefab];
+
+		// scale it to fill the screen based on current
+		// number of rows and columns
+		scalePrefab (prefab);
+
+		// STUDENT CODE TO SPAWN PREFABS GOES HERE
+
+
+		// STUDENT CODE TO SAFELY INCREASE PREFAB INDEX GOES HERE
+
+
+	}
+
+	// Edge Trigger Collider at bottom of screen
+	void OnTriggerEnter2D(Collider2D otherObject)
+	{
+		// if the monkey has hit the invisible edge at the bottom of the screen
+		if (otherObject.gameObject.name == "Monkey")
+		{
+			// update game over text
+			gameOverText.text = "Game Over!";
+
+			// delete the Monkey and the trampoline
+			Destroy(otherObject.gameObject);
+
+			GameObject trampoline = GameObject.Find ("Trampoline");
+			Destroy (trampoline);
+		}
+	}
+
+	// this utility function will return true if any objects
+	// with the "block" tag are left on the screen
+	bool anyBlocksLeft()
+	{
+		// STUDENT CODE TO FIND IF ALL BLOCKS ARE GONE GOES HERE
+
+
+		// no block found
+		return false;
+	}
+
+	// This utility function will calculate the size, starting positions,
+	// and offsets necessary to draw the current prefab block on the screen 
+	// and cover the entire width from left to right.
+	void scalePrefab (GameObject prefab)
+	{
+		// calculate the width of the screen
+		float worldHeight = Camera.main.orthographicSize * 2.0F;
+		float worldWidth = worldHeight * Camera.main.aspect;
+
+		// calculate the width of one block so the # of blocks per row fills the screen
+		float targetBlockWidth = worldWidth / blocksPerRow;
+
+		// reset any previous scaling that was done to this prefab
+		prefab.transform.localScale = new Vector3 (1.0F, 1.0F, 0);
+
+		// find the normal (unscaled) width
+		Renderer r = prefab.GetComponent<Renderer>();
+		float originalBlockWidth = r.bounds.size.x;
+		float originalBlockHeight = r.bounds.size.y;
+
+		// calculate the scaling factor to reach desired width
+		float scaleFactor = targetBlockWidth / originalBlockWidth;
+
+		// update the X and Y scaling factor to the same new value
+		prefab.transform.localScale = new Vector3(scaleFactor, scaleFactor, 0);
+
+		// calculate the xOffset and yOffset needed to neatly stack blocks
+		// of the new size across the screen
+		xOffset = targetBlockWidth;
+		yOffset = originalBlockHeight * scaleFactor;
+
+		// calculate upper-left starting location for first block
+		startingX = (-worldWidth / 2.0F) + (xOffset / 2.0F);
+		startingY = (worldHeight / 2.0F) - (yOffset / 2.0F);
+	}
+}
---
+++ Assets/Scripts/MonkeyScript.cs
@@ -0,0 +1,86 @@
+﻿/* Unity Game Programming
+ * Banana Breakout
+ * Author name
+ * Project date
+ */
+
+using UnityEngine;
+using TMPro;
+
+public class MonkeyScript : MonoBehaviour 
+{
+	// Initial movement Speed
+	public float speed = 4.0f;
+
+	// score and text display
+	private int score = 0;
+	public TextMeshProUGUI scoreText;
+
+	private float paddleWidth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// reset score and display
+		score = 0;
+		scoreText.text = "Score: " + score;
+
+		// grab the paddle width for future use
+		GameObject paddle = GameObject.Find("Trampoline");
+		paddleWidth = paddle.GetComponent<BoxCollider2D> ().size.x;
+
+		// calculate random inital direction for the monkey
+		// (1.0F units "up", + or - 0.5F units "left/right"
+		Vector2 initialDirection = new Vector2 (Random.value - 0.5F, 1.0F);
+		initialDirection.Normalize (); // get unit vector of consistent length 1
+
+		// set initial velocity in the initial direction at starting speed
+		GetComponent<Rigidbody2D>().linearVelocity = initialDirection * speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D otherObject) 
+	{
+		// This function is called whenever the ball
+		// collides with something
+
+		// Did we hit the trampoline?
+		if (otherObject.gameObject.name.Equals("Trampoline") )
+		{
+			// Calculate direction we'll bounce away from trampoline.
+			// If you hit near the left side, you'll bounce towards the left,
+			// If you hit near the right side, you'll bounce towards the right,
+			// If you hit near the middle, you'll bounce straight up.
+
+			// calculate the difference in X locations of center of mass
+			float xDifference = transform.position.x - otherObject.transform.position.x;
+			float xDirection = xDifference / paddleWidth;
+
+			// Calculate new direction, normalized to length to 1
+			Vector2 dir = new Vector2 (xDirection, 1).normalized;
+			
+			// Set new velocity equal to new direction * current speed
+			GetComponent<Rigidbody2D> ().linearVelocity = dir * speed;
+
+			// Every time we collide with the trampoline, check the Controller
+			// script and see if all of the blocks need to be regenerated.
+			// If blocks were regenerated, also increase the current monkey
+			// speed by a little bit to make the game harder.
+			Controller controllerScript = GameObject.Find ("Controller").GetComponent<Controller> ();
+			if (controllerScript.CheckRestart ()) 
+			{
+				speed += 1.0F; // speed up the monkey
+			}
+		} 
+		else if (otherObject.gameObject.CompareTag ("block")) 
+		{
+			// monkey collided with a block.
+
+			// increase the score
+			score++;
+			scoreText.text = "Score: " + score;
+
+			// destroy the block
+			Destroy (otherObject.gameObject);
+		}
+	}
+}
